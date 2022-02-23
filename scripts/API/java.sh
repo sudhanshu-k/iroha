721c511a6937d75f568681d50207c10e0e3e32c6
Aleksandr <a-p-petrosyan@yandex.ru>
2022-02-23 11:51:18 +0400

    [feature] #1726: Implement a release PR workflow.
    Signed-off-by: Aleksandr <a-p-petrosyan@yandex.ru>

--- scripts/API/java.sh
+++ scripts/API/java.sh
@@ -4,13 +4,13 @@ case $1 in
     setup)
         git clone https://github.com/hyperledger/iroha-java.git
         cd iroha-java || exit
-        git checkout iroha2-dev # TODO: chagnge this to main after release.
+        git checkout iroha2-dev # TODO: change this to main after release.
     ;;
     run)
         cd iroha-java || exit
         ./gradlew build
         ;;
     cleanup)
-        rm -rf iroha_api_test
+        rm -rf iroha_java
         ;;
 esac
